20494f7d0d4a00f1898849c54e16fe626d3cb353
birlikkoshan <birlikkoshan14@gmail.com>
2026-01-22 11:52:27 +0500

    b

--- app.js
+++ app.js
@@ -234,7 +234,7 @@ async function start() {
   });
 
   app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on ${PORT}`);
   });
 }
 
